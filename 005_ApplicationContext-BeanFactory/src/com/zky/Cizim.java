2077e602cede679372f3b40c9f9f589f6b3f0c3f
zekiyecakiral <zekiyecakiral@gmail.com>
2018-04-24 11:47:15 +0300

    ApplicationContext kullanımı

---
+++ 005_ApplicationContext-BeanFactory/src/com/zky/Cizim.java
@@ -0,0 +1,32 @@
+ package com.zky;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class Cizim {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext( new String[] {"spring.xml"});
+		
+		/*
+		 * * Bir projede birden çok Spring yapılandırma dosyalarıyla çalışma imkanı sağlar.
+		ApplicationContext context = new ClassPathXmlApplicationContext( new String[] {"spring.xml",
+		"spring2.xml",
+		"spring3.xml",
+		"spring4.xml"
+		});
+		  */
+		
+		BeanFactory beanFactory = context;
+		
+		Ucgen ucgen = (Ucgen) beanFactory.getBean("ucgen");
+		ucgen.ciz();
+		
+	
+	}
+	
+}
