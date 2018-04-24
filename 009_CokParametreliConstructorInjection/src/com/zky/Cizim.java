4668fbaeb43881d5000f40b9e1c6f3328f7aaf11
zekiyecakiral <zekiyecakiral@gmail.com>
2018-04-24 14:04:44 +0300

    CokParametreliConstructorInjection

---
+++ 009_CokParametreliConstructorInjection/src/com/zky/Cizim.java
@@ -0,0 +1,21 @@
+package com.zky;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class Cizim {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		Ucgen ucgen = (Ucgen) context.getBean("ucgen");
+		ucgen.ciz();
+		
+		
+	}
+	
+}
