1edd83b13f90dc94639b5a13007427dcdf3f9f41
zekiyecakiral <zekiyecakiral@gmail.com>
2018-04-24 14:01:36 +0300

    BirParametreliConstructorInjection

---
+++ 008_BirParametreliConstructorInjection/src/com/zky/Ucgen.java
@@ -0,0 +1,25 @@
+package com.zky;
+
+public class Ucgen {
+	
+	private String cesit;
+	
+	public Ucgen(String cesit) {
+		this.cesit=cesit;
+		
+	}
+	
+	public void setCesit(String cesit) {
+		this.cesit = cesit;	
+	}
+	public String getCesit() {
+		return cesit;
+	}
+	
+	
+	public void ciz() {
+		System.out.println( getCesit()+" Üçgen çiz");
+	}
+
+
+}
