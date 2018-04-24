28f61b4c325ec113b89084ec7a1c4a489eeaf5a2
zekiyecakiral <zekiyecakiral@gmail.com>
2018-04-24 13:56:04 +0300

    PropertyInitialization

---
+++ 006_ApplicationContext/src/com/zky/Ucgen.java
@@ -0,0 +1,10 @@
+package com.zky;
+
+public class Ucgen {
+	
+	public void ciz() {
+		System.out.println("Üçgen çiz");
+	}
+
+
+}
---
+++ 007_PropertyInitialization/src/com/zky/Ucgen.java
@@ -0,0 +1,21 @@
+package com.zky;
+
+public class Ucgen {
+	
+	private String cesit;
+	
+	public void setCesit(String cesit) {
+		this.cesit = cesit;	
+	}
+	public String getCesit() {
+		return cesit;
+	}
+	
+	
+	public void ciz() {
+	
+		System.out.println( getCesit()+" Üçgen çiz");
+	}
+
+
+}
