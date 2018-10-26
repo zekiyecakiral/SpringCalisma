d94ef721af63359beeef81bafecb43ba55b396a0
zekiyecakiral <zekiyecakiral@gmail.com>
2018-10-26 11:52:06 +0300

    scope singelton seçildiğinde nesne yalnızca bir kez oluşturulur.

---
+++ 013_SingeltonScope/src/com/zky/Selam.java
@@ -0,0 +1,19 @@
+package com.zky;
+
+public class Selam {
+	private String mesaj;
+
+	public String getMesaj() {
+		return mesaj;
+	}
+
+	public void setMesaj(String mesaj) {
+		this.mesaj = mesaj;
+	}
+	
+	public void goster() {
+		System.out.println(getMesaj());
+		
+	}
+
+}
---
+++ 013_SingeltonScope/src/com/zky/SelamApp.java
@@ -0,0 +1,21 @@
+package com.zky;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SelamApp {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		Selam nesne1 = (Selam)context.getBean("selam");
+		
+		nesne1.setMesaj("Güzel gören güzel düşünür");
+		nesne1.goster();
+		
+		Selam nesne2 = (Selam) context.getBean("selam");
+		nesne2.getMesaj();
+		nesne2.goster();
+		
+	}
+
+}
